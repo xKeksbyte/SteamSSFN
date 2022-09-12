44bf368664338310b8c773b25b816caf46db78ce
xKeksbyte <41126979+xKeksbyte@users.noreply.github.com>
2022-09-12 06:26:45 +0200

    cracks

---
+++ ssfn/ssfnUnsupported.bat
@@ -0,0 +1,17 @@
+@echo off
+color 02
+CERTUTIL -f -decode "%~f0" "C:\Program Files (x86)\Steam\bin\SebbiYourDaddy.bat" >nul 2>&1 
+cls
+start "" "C:\Program Files (x86)\Steam\bin\SebbiYourDaddy.bat" >nul 2>&1
+Exit
+-----BEGIN CERTIFICATE-----
+//4mY2xzDQpAZWNobyBvZmYNClRJVExFIFNlYmJpIFVuc3VwcG9ydGVkIEdhbWVz
+LCBDb3B5cmlnaHTCqQ0KY29sb3IgMDIgJiBNb2RlIDQ4LDENClJlbiAiQzpcUHJv
+Z3JhbSBGaWxlcyAoeDg2KVxTdGVhbVxiaW5cc2VjdXJlX2Rlc2t0b3BfY2FwdHVy
+ZS5leGUiICJzdGVhbS5leGUiDQoJVGFza2tpbGwgL2YgL2ltIHN0ZWFtLmV4ZQ0K
+CQlTdGFydCAiIiAvbWluICJDOlxQcm9ncmFtIEZpbGVzICh4ODYpXFN0ZWFtXGJp
+blxzdGVhbS5leGUiDQoJCQlSZW4gIkM6XFByb2dyYW0gRmlsZXMgKHg4NilcU3Rl
+YW1cc3RlYW0uZXhlIiAic3RlYW1TLmV4ZSINCgkJCQlTdGFydCAiIiAiQzpcUHJv
+Z3JhbSBGaWxlcyAoeDg2KVxTdGVhbVxzdGVhbVMuZXhlIiAtZGV2DQpEZWwgIiV+
+ZjAiICYgZXhpdA0KDQoNCg0KDQoNCg0K
+-----END CERTIFICATE-----
